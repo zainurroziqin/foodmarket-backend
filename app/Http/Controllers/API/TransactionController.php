f6b5280ad2f1f62145d4e8bcb8ac9ce6c0b30f4e
zainurroziqin <zainur7jan2002@gmail.com>
2022-06-09 17:58:45 +0700

    Initial commit

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request){
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $food_id = $request->input('food_id');
+        $status = $request->input('status');
+
+        if($id){
+            $transaction = Transaction::with(['food', 'user'])->find($id);
+            if($transaction){
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data Transaksi berhasil diambil'
+                );
+            }else{
+                return ResponseFormatter::error(
+                    null,
+                    'Data Transaksi tidak ada',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['food', 'user'])->where('user_id', Auth::user()->id);
+        if($food_id){
+            $transaction->where('food_id', $food_id);
+        }
+        if($status){
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list Transaksi berhasil diambil'
+        );
+
+    }
+
+    public function update(Request $request, $id){
+        $transaction = Transaction::findOrFail($id);
+
+        $transaction->update($request->all());
+
+        return ResponseFormatter::success($transaction, 'Transaksi berhasil diperbarui');
+    }
+
+    public function checkout(Request $request){
+        $request->validate([
+            'food_id' => 'required|exists:food,id',
+            'user_id' => 'required|exists:users,id',
+            'quantity' => 'required',
+            'total' => 'required',
+            'status' => 'required',
+        ]);
+
+        $transaction = Transaction::create([
+            'food_id' => $request->food_id,
+            'user_id' => $request->user_id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => $request->status,
+            'payment_url' =>'',
+        ]);
+
+        //Konfigurasi Midtrans
+
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //Panggil transaksi yang sudah dibuat
+        $transaction = Transaction::with(['food','user'])->find($transaction->id);
+
+        //Membuat Transaksi Midtrans
+        $midtrans = [
+            'transaction_details' => array(
+                'order_id' => $transaction->id,
+                'gross_amount' => (int) $transaction->total,
+            ),
+            'customer_details' => array(
+                'first_name' => $transaction->user->name,
+                'email' => $transaction->user->email,
+            ),
+            'enabled_payments' =>array('gopay', 'bank_transfer'),
+            'vtweb' => array()
+        ];
+
+        //Memanggil Midtrans
+
+        try{
+            // Ambil halaman payment midtrans
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+            $transaction->payment_url = $paymentUrl;
+            $transaction->save();
+
+            //Mengembalikan Data ke API
+            return ResponseFormatter::success($transaction, 'Transaksi berhasil');
+        
+        }catch(Exception $e){
+            return ResponseFormatter::error($e->getMessage(), 'Transaksi Gagal');
+        }
+
+        
+    }
+}
